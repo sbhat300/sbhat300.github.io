2f7a9a47399947c203dbdd4240ec27ac3be7f7cb
sbhat300 <87624722+sbhat300@users.noreply.github.com>
2025-09-06 22:51:43 -0400

    Fixed youtube video links

--- frontend/src/components/ProjectsPage/ProjectsPage.jsx
+++ frontend/src/components/ProjectsPage/ProjectsPage.jsx
@@ -15,7 +15,7 @@ const ProjectsPage = () => {
     thumbnail: RenderingThumbnail,
     title: "Rendering Engine",
     description: "3D rendering engine from scratch in C++ with OpenGL",
-    video: "https://www.youtube.com/embed/DOeeO616BBA",
+    video: "https://www.youtube.com/embed/E1R3naTwWPI",
     extendedDescription: "Wrote a 3D rendering engine from scratch in C++ with OpenGL. Implemented Blinn-Phong lighting with support " +
     "for specular, normal, and emission maps. Also implemented post-processing effects (bloom, HDR, blur), anti-aliasing, skyboxes, and " +
     "efficient model loading with face culling. I also ensured the system was modular and easily extensible for future features.",
@@ -25,7 +25,7 @@ const ProjectsPage = () => {
     thumbnail: PhysicsThumbnail,
     title: "Physics Engine",
     description: "Constraint based 2D physics engine using C++ and OpenGL",
-    video: "https://www.youtube.com/embed/F3CWYdUs4Tk",
+    video: "https://www.youtube.com/embed/AH2JXly5fUU",
     extendedDescription: "Made a constraint based 2D physics engine using C++ and OpenGL. Used concepts from linear algebra, multivariable calculus, " + 
     "and physics to create accurate physics simulations. Learned about Projected Gauss-Seidel, sequential impulses, and various other techniques " + 
     "such as warm starting, block solvers, and fixed timesteps. Made a collision detection program capable of detecting collisions and generating " +
@@ -37,7 +37,7 @@ const ProjectsPage = () => {
     thumbnail: NeuralNetworkThumbnail,
     title: "Neural Network",
     description: "Generalizable neural network in C++ written from scratch",
-    video: "https://www.youtube.com/embed/Lx3Fr4VxyWQ",
+    video: "https://www.youtube.com/embed/OczPagfIS1A",
     extendedDescription: "Created a neural network with C++. I made it generalizable so the number of input neurons, output neurons, hidden " + 
     "layers, number of hidden layer neurons, and other parameters are easily configurable. I also implemented backpropagation with gradient descent " +
     "to train the network along with momentum, mini-batch processing, and Kaiming Initialization to optimize training.",
@@ -56,7 +56,7 @@ const ProjectsPage = () => {
     thumbnail: LLMThumbnail,
     title: "PyTorch GPT",
     description: "GPT notebook with a lot of notes following Andrej Karpathy's tutorial",
-    video: "https://www.youtube.com/embed/H41NgwlAuLs",
+    video: "https://www.youtube.com/embed/RWG_eQ0UguQ",
     extendedDescription: "Programmed a GPT from scratch using PyTorch. Learned about transformers, embeddings, loss functions, feed forward layers, " +
     "blocks, residual connections, and more. This was made following Andrej Karpathy's tutorial on building a GPT from scratch, but almost every " +
     "line of code as comments explaining what it does, even for things like PyTorch functions.",
@@ -66,7 +66,7 @@ const ProjectsPage = () => {
     thumbnail: JOTR2Thumbnail,
     title: "Jai On The Run II",
     description: "Horror game made with Unity based off of my friend",
-    video: "https://www.youtube.com/embed/ZYkeZN7mHI30",
+    video: "https://www.youtube.com/embed/CpK6p8EO3Ec",
     extendedDescription: "3D horror game made with Unity. This project was a collaboration between me, and another friend who is really good at " +
     "making music. The game's two main character's are based off of and voice acted by two of my friends. The game featuers a tutorial along with " +
     "a core gameplay loop, and has lighting effects and enemy AI.",
@@ -76,7 +76,7 @@ const ProjectsPage = () => {
     thumbnail: EmailSenderThumbnail,
     title: "Emailer Bot",
     description: "A bot that emails you every morning with information",
-    video: "https://www.youtube.com/embed/5Sz-c8hWrGU",
+    video: "https://www.youtube.com/embed/Lh6viFHhYf8",
     extendedDescription: "This was one of my first projects ever made, and uses SMTP and Python to automatically send emails every morning. " +
     "It also uses IMAP to read emails, and you can send emails to it to remind you about things the next morning or use commands to add things like " +
     "birthdays. It also webscrapes with the requests library to get weather data.",
