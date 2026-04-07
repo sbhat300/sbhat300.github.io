56bbcbf4e7669894d6cd771b9f3afa753661ff98
Shreyas <sbhatkande100@gmail.com>
2026-04-07 18:14:23 -0400

    modular links

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,25 @@ import 'animate.css';
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const links = [
+    {
+      title: "About",
+      link: "/about"
+    },
+    {
+      title: "Projects",
+      link: "/projects"
+    },
+    {
+      title: "Experience",
+      link: "/experience"
+    },
+    {
+      title: "Contact",
+      link: "/contact"
+    }
+  ]
+
   return (
     <header className="header">
         <nav>
@@ -19,46 +38,19 @@ const Navbar = () => {
                 </span>
             </div>
 
+            
             <div className="nav-link-right">
-                <span className="nav-link-hover">
-                    <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="about-link nav-link animate__animated animate__fadeInDown animate-slow animate__delay-1"
-                    to="/about">
-                        <span className="nav-text">About</span>
-                    </NavLink>
-                </span>
-
-                <span className="nav-link-hover">
-                    <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="projects-link nav-link animate__animated animate__fadeInDown animate-slow animate__delay-2"
-                    to="/projects">
-                        <span className="nav-text">Projects</span>
-                    </NavLink>
-                </span>
-
-                <span className="nav-link-hover">
-                    <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="experience-link nav-link animate__animated animate__fadeInDown animate-slow animate__delay-3"
-                    to="/experience">
-                        <span className="nav-text">Experience</span>
-                    </NavLink>
-                </span>
-
-                <span className="nav-link-hover">
-                    <NavLink
-                    exact="true"
-                    activeclassname="active"
-                    className="contact-link nav-link animate__animated animate__fadeInDown animate-slow animate__delay-4"
-                    to="/contact">
-                        <span className="nav-text">Contact</span>
-                    </NavLink>
-                </span>
+                {links.map((data, index) => (
+                    <span className="nav-link-hover">
+                        <NavLink
+                        exact="true"
+                        activeclassname="active"
+                        className={`nav-link animate__animated animate__fadeInDown animate-slow animate__delay-${index + 1}`}
+                        to={data.link}>
+                            <span className="nav-text">{data.title}</span>
+                        </NavLink>
+                    </span>
+                ))}
             </div>
         </nav>
     </header>
--- frontend/src/components/VerticalNavbar/VerticalNavbar.jsx
+++ frontend/src/components/VerticalNavbar/VerticalNavbar.jsx
@@ -3,60 +3,46 @@ import './VerticalNavbar.scss'
 import { NavLink } from 'react-router-dom'
 
 const VerticalNavbar = ({onNavbarClose}) => {
+  const links = [
+    {
+      title: "Shreyas",
+      link: "/"
+    },
+    {
+      title: "About",
+      link: "/about"
+    },
+    {
+      title: "Projects",
+      link: "/projects"
+    },
+    {
+      title: "Experience",
+      link: "/experience"
+    },
+    {
+      title: "Contact",
+      link: "/contact"
+    }
+  ]
+
   return (
     <>
       <div className="vertical-navbar-container" onClick={onNavbarClose}>
         <button className="navbar-close-btn">
           &times;
         </button>
-        <span className="nav-link-hover">
-            <NavLink
-            exact="true"
-            activeclassname="active"
-            className="home-link nav-link animate__animated animate__fadeInLeft animate-slow"
-            to="/">
-                <span className="nav-text">Shreyas</span>
-            </NavLink>
-        </span>
-        <span className="nav-link-hover">
-          <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link nav-link animate__animated animate__fadeInLeft animate-slow animate__delay-1"
-          to="/about">
-              <span className="nav-text">About</span>
-          </NavLink>
-      </span>
-
-      <span className="nav-link-hover">
-          <NavLink
-          exact="true"
-          activeclassname="active"
-          className="projects-link nav-link animate__animated animate__fadeInLeft animate-slow animate__delay-2"
-          to="/projects">
-              <span className="nav-text">Projects</span>
-          </NavLink>
-      </span>
-
-      <span className="nav-link-hover">
-          <NavLink
-          exact="true"
-          activeclassname="active"
-          className="experience-link nav-link animate__animated animate__fadeInLeft animate-slow animate__delay-3"
-          to="/experience">
-              <span className="nav-text">Experience</span>
-          </NavLink>
-      </span>
-
-      <span className="nav-link-hover">
-          <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link nav-link animate__animated animate__fadeInLeft animate-slow animate__delay-4"
-          to="/contact">
-              <span className="nav-text">Contact</span>
-          </NavLink>
-      </span>
+        {links.map((data, index) => (
+          <span className="nav-link-hover">
+              <NavLink
+              exact="true"
+              activeclassname="active"
+              className={`nav-link animate__animated animate__fadeInLeft animate-slow animate__delay-${index}`}
+              to={data.link}>
+                  <span className="nav-text">{data.title}</span>
+              </NavLink>
+          </span>
+        ))}
       </div>
     </>
   )
